9181b4302fd6bfa7a381c13f64f1142e8f15fab8
nbutterly <butterlyn888@gmail.com>
2023-12-30 23:51:35 +0800

    docs: plain package path url, without "init" or "index"

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -2,9 +2,7 @@
 Generate the code reference pages and navigation.
 Script was derived from [mkdocstrings recipe](https://mkdocstrings.github.io/recipes/)
 """
-
 from pathlib import Path
-
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
@@ -12,16 +10,17 @@ nav = mkdocs_gen_files.Nav()
 src = Path(__file__).parent.parent / "src"
 
 for path in sorted(src.rglob("*.py")):
-    module_path = path.relative_to(src).with_suffix("")
+    module_path = path.relative_to(src)
+    module_path_without_suffix = module_path.with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
-    parts = tuple(module_path.parts)
+    parts = tuple(module_path_without_suffix.parts)
 
-    if parts[-1] == "__init__":  # rename __init__.py files to init.md
+    if parts[-1] == "__init__":  # make __init__.py files the package index
         parts = parts[:-1]
-        doc_path = doc_path.with_name("init.md")
-        full_doc_path = full_doc_path.with_name("init.md")
+        doc_path = doc_path.parent.with_suffix(".md")
+        full_doc_path = full_doc_path.parent.with_suffix(".md")
     elif parts[-1] == "__main__":  # skip __main__.py files
         continue
     # filter out private modules (i.e., those starting with _)
@@ -30,8 +29,10 @@ for path in sorted(src.rglob("*.py")):
 
     nav[parts] = doc_path.as_posix()
 
+    # Define the content of the page
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         ident = ".".join(parts)
+
         fd.write(f"::: {ident}")
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
